acd97210e9d2f5dda26644858651cb8afa45f084
root <abelbenardm@gmail.com>
2025-11-30 02:35:36 +0000

    improved the route builder

--- src/main/java/com/monitoringsystem/rest/Routes.java
+++ src/main/java/com/monitoringsystem/rest/Routes.java
@@ -1,10 +1,6 @@
 package com.monitoringsystem.rest;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import org.reflections.Reflections;
 
 import com.monitoringsystem.rest_handlers.user_access_mgmt.auth.ApiKeyAuthHandler;
 import com.monitoringsystem.rest_handlers.user_access_mgmt.auth.Rbac;
@@ -19,53 +15,40 @@ public class Routes
 {
     public static RoutingHandler routesRegister()
     {
-        Path baseDir = Paths.get("target/classes/com/monitoringsystem/rest_handlers");
-        String basePackage = "com.monitoringsystem.rest_handlers";
         RoutingHandler handlers = Handlers.routing();
 
-        try (Stream<Path> files = Files.walk(baseDir))
+        Reflections reflections = new Reflections("com.monitoringsystem.rest_handlers");
+
+        reflections.getTypesAnnotatedWith(EndpointProps.class).forEach(handlerClass -> 
         {
-            files.filter(p -> p.toString().endsWith(".class")).forEach(path -> 
+            try
             {
-                String relativePath = baseDir.relativize(path).toString();
-                String className = basePackage + "." + relativePath.replace("/", ".").replace("\\", ".").replace(".class", "");
-                
-                try
-                {
-                    Class<?> handlerClass = Class.forName(className);
-                    if (handlerClass.isAnnotationPresent(EndpointProps.class))
-                    {
-                        EndpointProps endpointProps = handlerClass.getDeclaredAnnotation(EndpointProps.class);
-                        String endpointPrefixPath = endpointProps.prefixPath();
-                        String endpointTemplatePath = endpointProps.templatePath();
-                        String fullPath = endpointPrefixPath + endpointTemplatePath;
-                        fullPath = fullPath.replaceAll("//", "/");
-                        String endpointMethod = endpointProps.httpMethod();
-                        String[] allowedRoles = endpointProps.allowedRoles();
+                EndpointProps endpointProps = handlerClass.getDeclaredAnnotation(EndpointProps.class);
+                String endpointPrefixPath = endpointProps.prefixPath();
+                String endpointTemplatePath = endpointProps.templatePath();
+                String fullPath = endpointPrefixPath + endpointTemplatePath;
+                fullPath = fullPath.replaceAll("//", "/");
+                String endpointMethod = endpointProps.httpMethod();
+                String[] allowedRoles = endpointProps.allowedRoles();
 
-                        HttpHandler handlerInstance = (HttpHandler) handlerClass.getDeclaredConstructor().newInstance();
+                HttpHandler handlerInstance = (HttpHandler) handlerClass.getDeclaredConstructor().newInstance();
 
-                        switch (endpointMethod)
-                        {
-                            case "GET" -> handlers.get(fullPath, new BlockingHandler(new ApiKeyAuthHandler(new Rbac(handlerInstance, allowedRoles))));
-                            case "POST" -> handlers.post(fullPath, new BlockingHandler(new ApiKeyAuthHandler(new Rbac(handlerInstance, allowedRoles))));
-                            case "PUT" -> handlers.put(fullPath, new BlockingHandler(new ApiKeyAuthHandler(new Rbac(handlerInstance, allowedRoles))));
-                            case "DELETE" -> handlers.delete(fullPath, new BlockingHandler(new ApiKeyAuthHandler(new Rbac(handlerInstance, allowedRoles))));
-                            default -> throw new IllegalArgumentException("Unsupported HTTP Method: " + endpointMethod);
-                        }
-                        System.out.println(fullPath);
-                    }
-                }
-                catch (Exception e)
+                switch (endpointMethod)
                 {
-                    e.printStackTrace();
+                    case "GET" -> handlers.get(fullPath, new BlockingHandler(new ApiKeyAuthHandler(new Rbac(handlerInstance, allowedRoles))));
+                    case "POST" -> handlers.post(fullPath, new BlockingHandler(new ApiKeyAuthHandler(new Rbac(handlerInstance, allowedRoles))));
+                    case "PUT" -> handlers.put(fullPath, new BlockingHandler(new ApiKeyAuthHandler(new Rbac(handlerInstance, allowedRoles))));
+                    case "DELETE" -> handlers.delete(fullPath, new BlockingHandler(new ApiKeyAuthHandler(new Rbac(handlerInstance, allowedRoles))));
+                    default -> throw new IllegalArgumentException("Unsupported HTTP Method: " + endpointMethod);
                 }
-            });
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+        });
+
 
         return handlers;
     }
